c4e320cabca53898459b58f0311ee665fe7a3657
karan-nigam96 <nigamkaran812@gmail.com>
2025-05-19 19:40:39 +0530

    implement interview generation

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const page = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent
+        userName="You"
+        userId="user1"
+        type="generate"
+      />
+    </>
+  )
+}
+
+export default page
